8b4d82e79c95247fa1bf06c55d716ac62ae9a014
Matvey <zxdfcvas4554@mail.ru>
2023-07-17 20:16:10 +0300

    Add new solution

--- Task31_String_Medium_ZigzagConversion/Task31_String_Medium_ZigzagConversion.cpp
+++ Task31_String_Medium_ZigzagConversion/Task31_String_Medium_ZigzagConversion.cpp
@@ -1,41 +1,73 @@
 #include <iostream>
+#include <vector>
+
+// It's second solution
 
 class Solution {
 public:
     static std::string convert(std::string s, int numRows) {
         if (numRows == 1)
             return s;
-
-        int in_row = (numRows * 2) - 2, n = s.length();
-        std::string result;
-
-        int count = 0;
+        std::vector<std::string> result(numRows);
+        int n = s.length(), count = 0, tmp = 0, direction = 1;
+        std::string res;
         while (count < n) {
-            result.push_back(s[count]);
-            count += in_row;
-        }
-
-        for (int i = 1;i < numRows - 1;i++) {
-            count = i;
-            while (count < n) {
-                result.push_back(s[count]);
-                count += (numRows - i - 1) * 2;
-                if (count < n) {
-                    result.push_back(s[count]);
-                    count += in_row - (numRows - i - 1) * 2;
-                }
+            std::cout << count;
+            result[tmp].push_back(s[count]);
+            if (tmp >= numRows - 1) {
+                direction = -1;
             }
+            if (tmp <= 0) {
+                direction = 1;
+            }
+            tmp += direction;
+            count++;
         }
-
-        count = numRows - 1;
-        while (count < n) {
-            result.push_back(s[count]);
-            count += in_row;
+        for (auto i : result) {
+            std::cout << i << std::endl;
         }
-
-        return result;
-    }
+        return res;
+    } // PAYPALISHIRING
 };
+
+// It's fast solution
+
+//class Solution {
+//public:
+//    static std::string convert(std::string s, int numRows) {
+//        if (numRows == 1)
+//            return s;
+//
+//        int in_row = (numRows * 2) - 2, n = s.length();
+//        std::string result;
+//
+//        int count = 0;
+//        while (count < n) {
+//            result.push_back(s[count]);
+//            count += in_row;
+//        }
+//
+//        for (int i = 1;i < numRows - 1;i++) {
+//            count = i;
+//            while (count < n) {
+//                result.push_back(s[count]);
+//                count += (numRows - i - 1) * 2;
+//                if (count < n) {
+//                    result.push_back(s[count]);
+//                    count += in_row - (numRows - i - 1) * 2;
+//                }
+//            }
+//        }
+//
+//        count = numRows - 1;
+//        while (count < n) {
+//            result.push_back(s[count]);
+//            count += in_row;
+//        }
+//
+//        return result;
+//    }
+//};
 
 int main()
 {
